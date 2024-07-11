fc12e1c236f057893b2c48cc2dbbe2e4b786e830
Space4QI <104172722+Space4QI@users.noreply.github.com>
2024-07-11 12:19:34 +0300

    Add files via upload

---
+++ lab2/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+package org.example
+
+const val num3: Int = 67
+fun main() {
+    val num: Int = 5
+    val let: Char = 'F'
+    val word: String = "Привет"
+    val num2: Double = 90.2
+    println(word)
+}
