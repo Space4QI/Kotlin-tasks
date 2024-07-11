38260beb8fac0972e8b4e330aad40da3e1acc03f
Space4QI <104172722+Space4QI@users.noreply.github.com>
2024-07-11 13:02:25 +0300

    Add files via upload

---
+++ lab24/src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+package org.example
+
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val arr = mutableListOf<Int>()
+    println("Введите числа массива. Если числа закончились - введите stop")
+    var input: String?
+    do {
+        input = readLine()
+        if (input != "stop") {
+            val num = input?.toIntOrNull() ?: throw IllegalArgumentException("Вы должны ввести число")
+            arr.add(num)
+        }
+    } while (input != "stop")
+
+   arr.sort()
+   println(arr)
+}
