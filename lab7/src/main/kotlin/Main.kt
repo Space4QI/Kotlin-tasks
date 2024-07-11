f1662522ff78e1b1223951ba09b73263948da63a
Space4QI <104172722+Space4QI@users.noreply.github.com>
2024-07-11 12:26:14 +0300

    Add files via upload

---
+++ lab7/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val num = 20 % 17
+    if(num > 0) {
+        println("Получилось значение большее чем 0")
+    }
+    else {
+        println("Получилось значение меньшее или равное 0")
+    }
+}
